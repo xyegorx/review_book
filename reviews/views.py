ad0a2c248f5e67c72c4fabf77f1af47158b13e22
xyegorx <smirnovyegor5@gmail.com>
2023-10-18 19:02:44 +0300

    добавили новые строки формы и т.д

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
-
+from django import forms
+from django.shortcuts import redirect
 # Create your views here.
+class ReviewForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    review = forms.CharField(widget=forms.Textarea)
+    rating = forms.IntegerField(min_value=1, max_value=5)
 
 def reviews(request):
-    name_1 = "Егор"
-    email_1 = "egorsmirnov316@gmail.com"
-    review_1 = "Всё отлично"
+    if request.method == 'GET':
+        form = ReviewForm()
 
+        return render(request, 'reviews.html', {
+            'form':form
+            })
 
-    return render(request, 'reviews.html', {"name_1":name_1, "email_1":email_1, "review_1":review_1})
+    elif request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            print(11111111111111111111111111111111111111111, data, 22222222222222222222222222222222222222222222)
+            name = data.get('name')
+            email = data.get('email')
+            review = data.get('review')
+            rating = data.get('rating')
+            with open('./data.csv','a',encoding="UTF-8")as file:
+                file.write(f'{name}|{email}|{review}|{rating}\n')
+            return redirect('reviews')
+
+        else:
+            print(333333333)
+            form = ReviewForm()
+            return render(request, 'reviews.html', {'form':form})
