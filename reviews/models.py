1728bd79b5977c52c1be3b0d97a1d2c0981468e6
xyegorx <smirnovyegor5@gmail.com>
2023-10-30 18:36:37 +0300

    добавили файлы регулярного выражения

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    rating = models.IntegerField()
+    reviews = models.TextField()
