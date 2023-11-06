51e3fe6bf76c4312529e24158ac3f185bbe3a66e
xyegorx <smirnovyegor5@gmail.com>
2023-11-06 18:21:31 +0300

    расширили меню для отзывов

--- reviews/models.py
+++ reviews/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
 # Create your models here.
 class Review(models.Model):
     def __str__(self):
         return f"Review by {self.name} <{self.email}>"
+
     name = models.CharField(max_length=25)
     email = models.EmailField()
 
-    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
     reviews = models.TextField()
